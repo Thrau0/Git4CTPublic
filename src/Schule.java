03abd160388bb319506145ce58dba1143f8de35c
Kev <stdejkev@bx.fallmerayer.it>
2023-05-15 08:22:06 +0200

    Morgen added

--- src/Schule.java
+++ src/Schule.java
@@ -2,6 +2,7 @@ public class Schule {
     String name;
     Schule(String name) {this.name = name;}
     public String greet (){
+        System.out.println("Mogen");
         return "Technologie" + "und" + "Planung: " + name;
-    }git
+    }
 }
