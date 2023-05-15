f9cf8ea05d93ad30f97ef195fa199d38d7a230d7
Kev <stdejkev@bx.fallmerayer.it>
2023-05-15 08:23:27 +0200

    Print added

--- src/Schule.java
+++ src/Schule.java
@@ -1,7 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Schule {
     String name;
     Schule(String name) {this.name = name;}
     public String greet (){
+        System.out.println("jakjhfhaldskjnfnlaskjnkjnnökjsn");
         return "Technologie" + "und" + "Planung: " + name;
-    }git
+    }
 }
