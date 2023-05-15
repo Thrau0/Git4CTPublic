4a5cb77d67b2d19e074bc3dcad90265c617284fb
Kev <stdejkev@bx.fallmerayer.it>
2023-05-15 08:17:18 +0200

    Schule Class erstellt

---
+++ src/Schule.java
@@ -0,0 +1,7 @@
+public class Schule {
+    String name;
+    Schule(String name) {this.name = name;}
+    public String greet (){
+        return "Technologie" + "und" + "Planung: " + name;
+    }git
+}
